a3c7181451b4189f75eb1671a03b0386f206770b
takafumi.m <taka.youthful.426@gmail.com>
2018-05-06 02:20:29 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only:[:show,:edit,:update,:destroy]
 
   def index
     #他のファイルでも使用する場合は、「@」のインスタンス変数を使用する 
@@ -6,7 +7,6 @@ class MessagesController < ApplicationController
   end
 
   def show
-     @message = Message.find(params[:id])
   end
 
   def new
@@ -29,12 +29,9 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
-
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -45,17 +42,18 @@ class MessagesController < ApplicationController
   end
 
   def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
-
-    flash[:success] = 'Message は正常に削除されました'
-    redirect_to messages_url
   end
   
   #このクラス内でのみ使用する
   private
+  def set_message
+    @message = Message.find(params[:id])  
+  end
+
   #Strong parameter
   def message_params
     params.require(:message).permit(:content)
   end
 end
+
+
